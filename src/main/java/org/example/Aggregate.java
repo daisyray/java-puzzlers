27fe1c036c36c6927ae89d0f02196fda6133e44c
Daisy Ray <raydaisy@gmail.com>
2023-11-27 15:17:24 -0500

    added new puzzlers

---
+++ src/main/java/org/example/Aggregate.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Aggregate {
+
+    int min;
+    int max;
+    double average;
+
+    public Aggregate(int min, int max, double average) {
+        this.min = min;
+        this.max = max;
+        this.average = average;
+    }
+
+    @Override
+    public String toString() {
+        return "Aggregate{" +
+                "min=" + min +
+                ", max=" + max +
+                ", average=" + average +
+                '}';
+    }
+}
